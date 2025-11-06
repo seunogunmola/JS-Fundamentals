3519d18e33c118feb419db74d4075c8327660f48
SEUN OGUNMOLA <ogunmola.net@gmail.com>
2025-11-06 16:24:13 +0100

    Updated 7-multi_c.js

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -4,9 +4,7 @@ let times = args[0];
 if(isNaN(times)){
     console.log("Missing number of occurrences");
 }
-else{
-    for(let i = 0; i < times; i++)
-    {
-        console.log("C is fun");
-    }
+for(let i = 0; i < times; i++)
+{
+    console.log("C is fun");
 }
