4bea3fdea08abef43bfdb6360c76d00126734827
SEUN OGUNMOLA <ogunmola.net@gmail.com>
2025-11-06 16:29:27 +0100

    Update 7-multi_c.js

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,10 +1,11 @@
 const args = process.argv.slice(2);
-let times = args[0];
-
-if(isNaN(times)){
+let count = args[0];
+if(isNaN(count)){
     console.log("Missing number of occurrences");
 }
-for(let i = 0; i < times; i++)
-{
+
+let i = 0;
+while(i < count){
     console.log("C is fun");
+    i++;
 }
